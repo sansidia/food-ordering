21c89da3243e1ef480d822448b41f86f7646d090
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2019-10-29 20:59:02 +0200

    read username password

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char username[20], password[20];
+
+    printf("Welcome to Food Thingies!\n");
+    printf("Please sign in to continue!\n");
+    printf("---Username:\n");
+    gets(username);
+    printf("---Password:\n");
+    gets(password);
+
+    printf("Username is %s and password is %s", username, password);
+
     return 0;
 }
