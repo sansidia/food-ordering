2f4e6195420d4cfeff3c16d99fc0535d3beaedc6
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2019-11-27 00:30:19 +0200

    parse full line of input

--- main.c
+++ main.c
@@ -4,11 +4,16 @@
 #include "parse-string.h"
 #include "userdata.h"
 #include "options.h"
+#include <ctype.h>
 #define NO_FOOD_TYPE 3
 #define NO_FOOD_SUBTYPE 4
 #define NO_DRINKS 4
-#define CHAR_LENGTH 20
+#define MAX_ITEM_LENGTH 20
 #define MAX_INPUT 100
+#define UNINITIALIZED_VALUE -1
+#define PREV_IS_TEXT 0
+#define PREV_IS_NR 1
+#define ERROR_MSG "Invalid input, please try again."
 int main() {
 
     //TODO: effective user inout management
@@ -20,35 +25,77 @@ int main() {
 
     char** foodTypes = (char**) malloc(noOfFoodTypes* sizeof(char*));
     char*** foodSubtypes = (char***) malloc(noOfFoodTypes* sizeof(char**));
-    int** noOfFoodSubtypes = (int**) malloc(noOfFoodTypes* sizeof(int*));
+    int* noOfFoodSubtypes = (int*) malloc(noOfFoodTypes* sizeof(int));
     double** foodSubtypePrices = (double**) malloc(noOfFoodTypes* sizeof(double*));
 
     char *userInput = (char*) malloc(MAX_INPUT * sizeof(char));
     for (int currentItem = 0; currentItem < noOfFoodTypes; ++currentItem) {
         gets(userInput);
         printf("Input is \"%s\"\n", userInput);
+        //COUNT SUBTYPES
+        noOfFoodSubtypes[currentItem] = 0;
+        for (unsigned int i = 0; i < strlen(userInput); ++i) {
+            if (userInput[i] == '(') noOfFoodSubtypes[currentItem]++;
+        }
+        foodSubtypes[currentItem] = (char**) malloc(noOfFoodSubtypes[currentItem] * sizeof(char*));
+        foodSubtypePrices[currentItem] = (double*) malloc(noOfFoodSubtypes[currentItem]* sizeof(double));
         //PARSE INPUT
         char* separator;
         char separatorString[] = ",() :;";
         separator = strtok(userInput, separatorString);
+        printf("separator is %s\n", separator);
+        foodTypes[currentItem] = (char*) malloc(MAX_ITEM_LENGTH * sizeof(char));
         strcpy(foodTypes[currentItem], separator);
-        printf("%s\n", foodTypes[currentItem]);
+        printf("foodtype is %s\n", foodTypes[currentItem]);
+        int currentSubtype = UNINITIALIZED_VALUE;
+        int previousStringType = UNINITIALIZED_VALUE;
+        while ((separator = strtok(NULL, separatorString))) {
+            printf("sep is %s\n", separator);
+            if (isalpha(separator[0])) {
+                if (previousStringType == UNINITIALIZED_VALUE) {
+                    currentSubtype++;
+                    previousStringType = PREV_IS_TEXT;
+                    foodSubtypes[currentItem][currentSubtype] = (char*) malloc(MAX_ITEM_LENGTH* sizeof(char));
+                    strcpy(foodSubtypes[currentItem][currentSubtype], separator);
+                    printf("%s type %d is %s\n", foodTypes[currentItem], currentSubtype, foodSubtypes[currentItem][currentSubtype]);
+                } else if (previousStringType == PREV_IS_TEXT) {
+                    strcat(foodSubtypes[currentItem][currentSubtype], " ");
+                    strcat(foodSubtypes[currentItem][currentSubtype], separator);
+                    printf("%s type %d is %s\n", foodTypes[currentItem], currentSubtype, foodSubtypes[currentItem][currentSubtype]);
+                    previousStringType = PREV_IS_TEXT;
+                } else if (previousStringType == PREV_IS_NR) {
+                    currentSubtype++;
+                    previousStringType = PREV_IS_TEXT;
+                    foodSubtypes[currentItem][currentSubtype] = (char*) malloc(MAX_ITEM_LENGTH* sizeof(char));
+                    strcpy(foodSubtypes[currentItem][currentSubtype], separator);
+                    printf("%s type %d is %s\n", foodTypes[currentItem], currentSubtype, foodSubtypes[currentItem][currentSubtype]);
+                }
+            } else if (isdigit(separator[0])) {
+                if (previousStringType == UNINITIALIZED_VALUE) {
+                    printf(ERROR_MSG);
+                } else if (previousStringType == PREV_IS_TEXT) {
+                    previousStringType = PREV_IS_NR;
+                    char** pVoid = NULL;
+                    foodSubtypePrices[currentItem][currentSubtype] = strtod(separator, pVoid);
+                    printf("%s type %d is %s and costs %.2f\n", foodTypes[currentItem], currentSubtype, foodSubtypes[currentItem][currentSubtype], foodSubtypePrices[currentItem][currentSubtype]);
+                } else if (previousStringType == PREV_IS_NR) {
+                    printf(ERROR_MSG);
+                }
+            }
+        }
     }
 
     //MEMORY DEALLOCATION
     for (int i = 0; i < noOfFoodTypes; ++i) {
         free(foodTypes[i]);
         free(foodSubtypePrices[i]);
-        for (int j = 0; j < *noOfFoodSubtypes[i]; ++j) {
+        for (int j = 0; j < noOfFoodSubtypes[i]; ++j) {
             free(foodSubtypes[i][j]);
         }
         free(foodSubtypes[i]);
     }
     free(foodSubtypePrices);
     free(foodSubtypes);
-    for (int k = 0; k < noOfFoodTypes; ++k) {
-        free(noOfFoodSubtypes[k]);
-    }
     free(noOfFoodSubtypes);
     free(foodSubtypes);
     free(userInput);
@@ -103,9 +150,9 @@ int main() {
 
 
 /*
-    char username[CHAR_LENGTH];
-    char password[CHAR_LENGTH];
-    char foodTypes[NO_FOOD_TYPE][CHAR_LENGTH] = {"Pizza", "Pasta", "Salad"};
+    char username[MAX_ITEM_LENGTH];
+    char password[MAX_ITEM_LENGTH];
+    char foodTypes[NO_FOOD_TYPE][MAX_ITEM_LENGTH] = {"Pizza", "Pasta", "Salad"};
     int foodTypeChoice = -1;
     int foodSubtypeChoice = -1;
     int drinkChoice = -2;
@@ -116,11 +163,11 @@ int main() {
         {23, 21},
         {23, 22, 19, 21} };
     int noOfFoodSubtypes[NO_FOOD_TYPE] = {3, 2, 4};
-    char foodSubtypes[NO_FOOD_TYPE][NO_FOOD_SUBTYPE][CHAR_LENGTH] = {
+    char foodSubtypes[NO_FOOD_TYPE][NO_FOOD_SUBTYPE][MAX_ITEM_LENGTH] = {
         {"Pizza con Pollo", "Pizza Diavola", "Pizza Margherita"},
         {"Chicken alfredo", "Mac&cheese"},
         {"Tuna Salad", "Chicken Salad", "Greek Salad", "Cobb"} };
-    char drinkOptions[NO_DRINKS][CHAR_LENGTH] = {"Coca-Cola", "Fanta", "Lipton", "Water"};
+    char drinkOptions[NO_DRINKS][MAX_ITEM_LENGTH] = {"Coca-Cola", "Fanta", "Lipton", "Water"};
     int drinkOptionPrices[NO_DRINKS] = {5, 5, 5, 4};
     char cutleryOptions[2][5] = {"Yes", "No"};
     char additionalInfo[100];
@@ -132,15 +179,15 @@ int main() {
                 state++; break;
             case 1: //food type selection
                 printf("Please choose your meal:\n");
-                printMenu(NO_FOOD_TYPE, CHAR_LENGTH, foodTypes);
+                printMenu(NO_FOOD_TYPE, MAX_ITEM_LENGTH, foodTypes);
                 foodTypeChoice = makeChoice(&state, NO_FOOD_TYPE, 1); break;
             case 2: //food subtype selection
                 printf("Please choose your favourite %s:\n", foodTypes[foodTypeChoice]);
-                printMenuWithPrices(0, noOfFoodSubtypes[foodTypeChoice], CHAR_LENGTH, foodSubtypes[foodTypeChoice], foodSubtypePrices[foodTypeChoice]);
+                printMenuWithPrices(0, noOfFoodSubtypes[foodTypeChoice], MAX_ITEM_LENGTH, foodSubtypes[foodTypeChoice], foodSubtypePrices[foodTypeChoice]);
                 foodSubtypeChoice = makeChoice(&state, noOfFoodSubtypes[foodTypeChoice], 1); break;
             case 3: //drink selection
                 printf("Please choose a drink to go with your %s:\n", foodTypes[foodTypeChoice]);
-                printMenuWithPrices(1, NO_DRINKS, CHAR_LENGTH, drinkOptions, drinkOptionPrices);
+                printMenuWithPrices(1, NO_DRINKS, MAX_ITEM_LENGTH, drinkOptions, drinkOptionPrices);
                 drinkChoice = makeChoice(&state, NO_DRINKS + 1, 1); break;
             case 4: //cutlery selection
                 printf("Do you want cutlery?\n");
