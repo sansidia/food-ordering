0d1e8c74ea811fc71c9c4621da585334c007defd
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2019-11-28 12:49:57 +0200

    bad commit, non functional program

---
+++ input_data.h
@@ -0,0 +1,14 @@
+//
+// Created by diana on 28/11/2019.
+//
+
+#ifndef FOOD_ORDERING_INPUT_DATA_H
+#define FOOD_ORDERING_INPUT_DATA_H
+
+int readNrOfTypes();
+void
+readData(int dataType, int nrOfGeneralTypes, char ***generalType, int **nrOfSubtypes, char ****subTypes,
+         double ***subTypePrices);
+
+
+#endif //FOOD_ORDERING_INPUT_DATA_H
---
+++ macros.h
@@ -0,0 +1,14 @@
+//
+// Created by diana on 28/11/2019.
+//
+
+#ifndef FOOD_ORDERING_MACROS_H
+#define FOOD_ORDERING_MACROS_H
+
+#define ITEM_LENGTH 25
+#define LONG_INPUT_LENGTH 120
+#define LOAD_DATA "Please load the data\n"
+#define DATA_TYPE_FOOD 0
+#define DATA_TYPE_DRINKS 1
+
+#endif //FOOD_ORDERING_MACROS_H
