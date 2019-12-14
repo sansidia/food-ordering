683f814f129abec2a1222bdfd549c9b07c08aff6
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2019-12-14 21:02:30 +0200

    sign up works

--- userdata.c
+++ userdata.c
@@ -7,6 +7,8 @@
 #include "options.h"
 #include "struct_type.h"
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 void printFormHeader(char*);
 double paymentSum(double foodPrice, double drinkPrice, int drinkCode, int noDrink);
@@ -50,11 +52,67 @@ int doesPasswordExist(char *username, struct user *registeredUsers, int nrOfUser
     return 0;
 }
 
-int signUp(struct user mUser, struct user *registeredUsers, int nrOfUsers) {
+int validatePassword (struct user user) {
+    setbuf(stdout, 0);
+    struct user auxUser;
+    auxUser.username = (char*) malloc(strlen(user.username)* sizeof(char));
+    auxUser.password = (char*) malloc(strlen(user.password)* sizeof(char));
+    auxUser = user;
+    if (strlen(user.password)<7) return 1;
+    strlwr(auxUser.username); strlwr(auxUser.password);
+    if (strstr(auxUser.password, auxUser.username)) return 4;
+    if (strchr(user.password, ',')==NULL && strchr(user.password, '!')==NULL && strchr(user.password, '_') ==NULL) return 3;
+    int nrOfDigits = 0;
+    for (int i = 0; i < strlen(user.password); ++i) {
+        if (isdigit(user.password[i])) nrOfDigits++;
+    }
+    if (nrOfDigits == 0) return 2;
+    return 0;
+}
 
+int signUp(struct user mUser, struct user *registeredUsers, int nrOfUsers) {
+    setbuf(stdout, 0);
+    int signUpComplete = 0;
+    getchar();
+    while (signUpComplete == 0) {
+        printf(SIGN_UP);
+        printf("---Username:\n");
+        gets(mUser.username);
+        printf("---Password:\n");
+        gets(mUser.password);
+        if (doesUsernameExist(mUser.username, registeredUsers, nrOfUsers)) {
+            printf(DUPLICATE_USER);
+        } else if (validatePassword(mUser) == 0){
+            return 1;
+        } else {
+            switch (validatePassword(mUser)) {
+                case 1: {
+                    printf(ERROR_PASSWORD_LONG);
+                    break;
+                }
+                case 2: {
+                    printf(ERROR_PASSWORD_DIGITS);
+                    break;
+                }
+                case 3: {
+                    printf(ERROR_PASSWORD_SPECIAL_CHAR);
+                    break;
+                }
+                case 4: {
+                    printf(ERROR_PASSWORD_NOT_USERNAME);
+                    break;
+                }
+                default: {
+                    printf("WTF did you do to get here?!\n");
+                    break;
+                }
+            }
+        }
+    }
 }
 
 void signInOrUp(struct user mUser, struct user *registeredUsers, int nrOfUsers) {
+    setbuf(stdout, 0);
     printf("Welcome to Food Thingies!\n");
     int signComplete = 0;
     while (signComplete == 0) {
